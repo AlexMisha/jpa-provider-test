26c16503f70f4d355dc3bf36399c97be501cd0a4
AlexMisha <michaelalex600@gmail.com>
2018-04-24 14:11:51 +0300

    init eclipse link project Конфиг eclipselink к javamelody

---
+++ eclipselink/src/main/kotlin/com/shepard/jpaprovidertest/eclipselink/EclipselinkApplication.kt
@@ -0,0 +1,16 @@
+package com.shepard.jpaprovidertest.eclipselink
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver
+
+@SpringBootApplication(scanBasePackages = ["com.shepard.jpaprovidertest.config"])
+class EclipselinkApplication {
+    @Bean
+    fun loadTimeWeaver() = InstrumentationLoadTimeWeaver()
+}
+
+fun main(args: Array<String>) {
+    runApplication<EclipselinkApplication>(*args)
+}
