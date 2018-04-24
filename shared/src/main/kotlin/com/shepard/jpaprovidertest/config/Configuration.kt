90e29faa86d37695a7ad94a8657a6ed20ac1decf
AlexMisha <michaelalex600@gmail.com>
2018-04-24 14:10:00 +0300

    Настройка javamelody для мониторинга провайдеров

--- shared/src/main/kotlin/com/shepard/jpaprovidertest/config/Configuration.kt
+++ shared/src/main/kotlin/com/shepard/jpaprovidertest/config/Configuration.kt
@@ -1,8 +1,22 @@
 package com.shepard.jpaprovidertest.config
 
+import net.bull.javamelody.JpaPersistence
 import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
+import java.util.*
+import javax.sql.DataSource
 
 @Configuration
 @EntityScan(basePackages = ["com.shepard.jpaprovidertest.entity"])
-class Configuration
+class Configuration {
+    @Bean
+    fun entityManagerFactory(dataSource: DataSource, properties: Properties) =
+            LocalContainerEntityManagerFactoryBean().apply {
+                setPackagesToScan("com.shepard.jpaprovidertest.entity")
+                setJpaProperties(properties)
+                persistenceProvider = JpaPersistence()
+                this.dataSource = dataSource
+            }
+}
