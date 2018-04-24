6cd87d68f62cb7c993b33bdfffe1a60886ff9ab7
AlexMisha <michaelalex600@gmail.com>
2018-04-24 14:10:42 +0300

    Конфиг hibernate к javamelody

--- hibernate/src/main/kotlin/com/shepard/jpaprovidertest/hibernate/HibernateApplication.kt
+++ hibernate/src/main/kotlin/com/shepard/jpaprovidertest/hibernate/HibernateApplication.kt
@@ -2,10 +2,23 @@ package com.shepard.jpaprovidertest.hibernate
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.core.env.Environment
+import java.util.*
 
 @SpringBootApplication(scanBasePackages = ["com.shepard.jpaprovidertest.config",
     "com.shepard.jpaprovidertest.hibernate.controller"])
-class HibernateApplication
+class HibernateApplication {
+    @Bean
+    fun properties(environment: Environment) = Properties().apply {
+        setProperty("hibernate.dialect", environment.getProperty("spring.jpa.properties.hibernate.dialect"))
+        setProperty("hibernate.connection.pool_size", "1")
+        setProperty("hibernate.show_sql", "true")
+        setProperty("hibernate.format_sql", "true")
+        setProperty("hibernate.temp.use_jdbc_metadata_defaults", "false")
+        setProperty("hibernate.statement_cache.size", "0")
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<HibernateApplication>(*args)
