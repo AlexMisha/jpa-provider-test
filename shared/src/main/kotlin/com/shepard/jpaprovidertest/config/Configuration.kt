0d8a9caf5ed189f5693d55925399533dfded3e0f
AlexMisha <michaelalex600@gmail.com>
2018-04-24 15:05:47 +0300

    Единый контроллер для тестов

--- shared/src/main/kotlin/com/shepard/jpaprovidertest/config/Configuration.kt
+++ shared/src/main/kotlin/com/shepard/jpaprovidertest/config/Configuration.kt
@@ -3,6 +3,7 @@ package com.shepard.jpaprovidertest.config
 import net.bull.javamelody.JpaPersistence
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
 import java.util.*
@@ -10,6 +11,7 @@ import javax.sql.DataSource
 
 @Configuration
 @EntityScan(basePackages = ["com.shepard.jpaprovidertest.entity"])
+@ComponentScan(basePackages = ["com.shepard.jpaprovidertest.controller"])
 class Configuration {
     @Bean
     fun entityManagerFactory(dataSource: DataSource, properties: Properties) =
