6878e422dc6a6d10504dce7a60ef7ca2f42d643f
AlexMisha <michaelalex600@gmail.com>
2018-05-11 12:53:57 +0300

    Фабрика result set

--- shared/src/main/kotlin/com/shepard/jpaprovidertest/tests/JpaTests.kt
+++ shared/src/main/kotlin/com/shepard/jpaprovidertest/tests/JpaTests.kt
@@ -1,17 +1,15 @@
 package com.shepard.jpaprovidertest.tests
 
 import com.shepard.jpaprovidertest.criteria.query
-import com.shepard.jpaprovidertest.driver.AbstractResultSet
+import com.shepard.jpaprovidertest.driver.resultSetOf
 import com.shepard.jpaprovidertest.entity.Account
 import com.shepard.jpaprovidertest.entity.Account_
 import org.springframework.stereotype.Component
 import java.sql.ResultSet
-import java.sql.ResultSetMetaData
 import java.sql.Types
 import java.util.logging.Logger
 import javax.persistence.EntityManager
 import javax.persistence.PersistenceContext
-import javax.sql.rowset.RowSetMetaDataImpl
 
 interface JpaTest {
     val name: String
@@ -21,10 +19,14 @@ interface JpaTest {
 
 @Component
 class AccountTest : JpaTest {
-    override val name: String = this.javaClass.simpleName
-    override val resultSet: ResultSet = AccountResultSet()
+    override val name: String = javaClass.simpleName
+    override val resultSet = resultSetOf(
+            Account::class.java,
+            mapOf("id" to 1L, "name" to "michael"),
+            listOf(Types.BIGINT, Types.VARCHAR)
+    )
 
-    private val logger: Logger = Logger.getLogger(this.javaClass.name)
+    private val logger: Logger = Logger.getLogger(javaClass.name)
 
     @PersistenceContext
     lateinit var entityManager: EntityManager
@@ -37,52 +39,5 @@ class AccountTest : JpaTest {
         }
         val result = entityManager.createQuery(query).singleResult
         logger.info("result: $result")
-    }
-
-    class AccountResultSet : AbstractResultSet() {
-        private val logger: Logger = Logger.getLogger(this.javaClass.name)
-        private var count = 2
-
-        override fun next(): Boolean {
-            count--
-            logger.info("next count: $count")
-            return count != 0
-        }
-
-        override fun getString(columnIndex: Int): String {
-            logger.info("getString columnIndex: $columnIndex")
-            return "michael"
-        }
-
-        override fun getString(columnLabel: String?): String {
-            logger.info("getString columnLabel: $columnLabel")
-            return "michael"
-        }
-
-        override fun getLong(columnIndex: Int): Long {
-            logger.info("getLong columnIndex: $columnIndex")
-            return 1L
-        }
-
-        override fun getLong(columnLabel: String?): Long {
-            logger.info("getLong columnLabel: $columnLabel")
-            return 1L
-        }
-
-        override fun getMetaData(): ResultSetMetaData {
-            return ResultSetMetaDataImpl(RowSetMetaDataImpl())
-        }
-    }
-
-    class ResultSetMetaDataImpl(resultSetMetaData: ResultSetMetaData) : ResultSetMetaData by resultSetMetaData {
-        var count = 1
-
-        override fun getColumnCount() = 2
-
-        override fun getColumnType(column: Int) = when (count++) {
-            1 -> Types.BIGINT
-            2 -> Types.VARCHAR
-            else -> Types.NULL
-        }
     }
 }
